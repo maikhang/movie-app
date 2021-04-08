a1fdfe08efd62c8bef634c892152f39cd2bd8e2e
Mai Khang <mhkhang.khmt0117@student.ctuet.edu.vn>
2021-04-08 15:32:06 +0700

    add movie index page

---
+++ app/Http/Controllers/MovieController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class MovieController extends Controller
+{
+    //[GET] INDEX
+    public function index($page=1){
+        $popularMovies = Http::withToken(config('services.tmdb.token'))
+                                ->get('https://api.themoviedb.org/3/movie/popular?page='.$page)
+                                ->json()['results'];
+
+        return view('client.movie.index', compact('popularMovies', 'page'));
+    }
+
+    //[GET] SHOW
+    public function show($id){
+        $movie = collect(Http::withToken(config('services.tmdb.token'))
+                    ->get('https://api.themoviedb.org/3/movie/'.$id.'?append_to_response=videos,images,casts')
+                    ->json())
+                    ->only(['videos', 'title', 'genres', 'name', 'vote_average', 'runtime', 'release_date', 'production_companies', 'overview', 'homepage', 'casts', 'images']);
+
+        // dd($movie['casts']['cast']);
+
+        return view('client.movie.show', compact('movie'));
+    }
+
+}
---
+++ resources/views/client/movie/index.blade.php
@@ -0,0 +1,57 @@
+@extends('client.master')
+
+@section('content')
+<div class="main-content">
+    {{-- Popular Movies --}}
+    <section id="iq-favorites">
+       <div class="container-fluid">
+          <div class="infinite-scroll row">
+               @foreach ($popularMovies as $popularMovie)
+                  <div class="col-md-3 col-sm-6 mb-3 item-scroll">
+                     <a href="{{ route('movie.show', $popularMovie['id']) }}">
+                        <div class="block-images position-relative">
+                           <div class="img-box" style="opacity: 0.4">
+                              @if(isset($popularMovie['poster_path']))
+                                 <img src="https://www.themoviedb.org/t/p/w342{{$popularMovie['poster_path']}}" class="img-fluid" alt="">
+                              @else
+                                 <img src="{{ asset('asset/html/frontend/html/images/not-found-image.jpg') }}" class="img-fluid" alt="">
+                              @endif
+                           </div>
+                           <div class="block-description text-center" style="opacity: 1">
+                              <h6>{{ $popularMovie['title'] }}</h6>
+                              <div class="movie-time d-flex align-items-center my-2 justify-content-center">
+                                 <div class="badge badge-secondary p-1 mr-2">{{ $popularMovie['vote_average'] * 10 . '%' }}</div>
+                                 <span class="text-white">
+                                    @if (isset($popularMovie['release_date']))
+                                    {{ Carbon\Carbon::parse($popularMovie['release_date'])->format('M d, Y')  }}
+                                    @endif
+                                 </span>
+                              </div>
+                              <div class="hover-buttons">
+                                 <span class="btn btn-hover">
+                                 <i class="fa fa-play mr-1" aria-hidden="true"></i>
+                                 Watch Now
+                                 </span>
+                              </div>
+                           </div>
+                        </div>
+                     </a>
+                  </div>
+               @endforeach
+             </div>
+          </div>
+       </div>
+    </section>
+</div>
+@endsection
+
+@section('scripts')
+<script src="{{ asset('asset/infinite-scroll/infinite-scroll.pkgd.min.js') }}"></script>
+<script>
+   $('.infinite-scroll').infiniteScroll({
+   // options
+   path: '/movies/page/@{{#}}',
+   append: '.item-scroll',
+});
+</script>
+@endsection
